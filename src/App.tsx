20204dbe5c5680b5779aecd596c622eedba69c64
Hugo Hsi <hhsi@hugodehsign.com>
2024-03-13 01:30:39 -0400

    added dark mode functionality to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Dashboard from '@/components/Dashboard';
 import Header from '@/components/Header';
+import { ThemeProvider } from '@/components/theme/theme-provider';
 
 function App() {
   return (
-    <div className="m-8 flex flex-col gap-8">
-      <Header />
-      <Dashboard />
-    </div>
+    <ThemeProvider
+      defaultTheme="dark"
+      storageKey="vite-ui=theme">
+      <div className="m-8 flex flex-col gap-8">
+        <Header />
+        <Dashboard />
+      </div>
+    </ThemeProvider>
   );
 }
 
