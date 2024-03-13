24cff7746a0addfa0449448a6287c29ff1fbf7f6
Hugo Hsi <hhsi@hugodehsign.com>
2024-03-13 06:37:53 -0400

    added history button functionality

--- src/components/citySelect/CityHistory.tsx
+++ src/components/citySelect/CityHistory.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { cityHistoryAtom } from '@/state';
+import { cityHistoryAtom, currentCityAtom } from '@/state';
 import { useAtom } from 'jotai';
 
 export default function CityHistory() {
   const [cityHistory, setCityHistory] = useAtom(cityHistoryAtom);
+  const [currentCity, setCurrentCity] = useAtom(currentCityAtom);
   return (
     <Card>
       <CardHeader>
@@ -12,13 +13,12 @@ export default function CityHistory() {
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
         {cityHistory.map((city) => (
-          <Button>{city.name}</Button>
+          <Button
+            key={JSON.stringify(city)}
+            onClick={() => setCurrentCity(city)}>
+            {city.name} - {city.state}
+          </Button>
         ))}
-        {/* <Button>test</Button>
-        <Button>test</Button>
-        <Button>test</Button>
-        <Button>test</Button>
-        <Button>test</Button> */}
       </CardContent>
     </Card>
   );
