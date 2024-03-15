74b2080afc1bd9d84879c3d16410bcd827e80809
Hugo Hsi <hhsi@hugodehsign.com>
2024-03-15 00:22:56 -0400

    removed permanent search dropdown open

--- src/components/citySelect/search/CitySearchCombobox.tsx
+++ src/components/citySelect/search/CitySearchCombobox.tsx
@@ -24,7 +24,7 @@ export function CitySearchCombobox() {
   return (
     <div className="z-20">
       <Select
-        open={true}
+        open={open}
         onOpenChange={() => {
           setOpen(!open);
         }}
